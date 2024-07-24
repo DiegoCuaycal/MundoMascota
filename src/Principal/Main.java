42bce57e4ecf0d52e1bdab512d05e96bbc10fc29
FerroCenter <diegocuaycal01@gmail.com>
2024-07-23 20:17:10 -0500

    Mundo Mascota

---
+++ src/Principal/Main.java
@@ -0,0 +1,18 @@
+package Principal;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal P = new Principal();
+        P.setVisible(true);
+
+        int a = 10;
+        for (int i = 0; i < a; i++) {
+            a--;
+            System.out.println(i);
+        }
+
+    }
+}
